60383b87a6b772d77a82b75ffa8ed1d187f896ac
Neeraj Gupta <ng3214277@gmail.com>
2025-11-25 13:56:15 +0530

    Add basic calculator functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# calculator.py
+# Simple demo for CA3
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("5 + 7 =", add(5, 7))
+    print("10 - 3 =", sub(10, 3))
